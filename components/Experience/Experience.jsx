fead1ad076fce4e4fb924f8ed161bb70ca637e80
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-01-09 20:57:08 +0530

    Models and environment map

--- components/Experience/Experience.jsx
+++ components/Experience/Experience.jsx
@@ -1,14 +1,15 @@
 import {Canvas} from "@react-three/fiber";
-import {TestComp, TestInstances} from "../TestComp/TestComp.jsx";
-import {BakeShadows, OrbitControls, PresentationControls} from "@react-three/drei";
+import {BakeShadows, Environment, OrbitControls} from "@react-three/drei";
 import {Perf} from "r3f-perf";
+import {TestComp2} from "../TestComp2/TestComp2.jsx";
+import {TestComp, TestInstances} from "../TestComp/TestComp.jsx";
 
 export default function Experience() {
     return (
         <Canvas
             camera={{
                 position: [2, 2, 2],
-                far: 10
+                // far: 10
             }}
         >
             <Perf position='top-left'/>
@@ -16,6 +17,13 @@ export default function Experience() {
             <OrbitControls
                 maxPolarAngle={Math.PI / 2.5}
                 minPolarAngle={Math.PI / 5}
+                // enablePan={false}
+                // enableZoom={false}
+            />
+
+            <Environment
+                files='/environments/kloppenheim_06_puresky_1k.hdr'
+                background={true}
             />
 
             <BakeShadows/>
@@ -23,25 +31,14 @@ export default function Experience() {
             {/*<fogExp2 attach="fog" color='#564d65' density={0.03}/>*/}
 
             <ambientLight intensity={2}/>
-            {/*<PresentationControls*/}
-            {/*    enabled={true} // the controls can be disabled by setting this to false*/}
-            {/*    global={false} // Spin globally or by dragging the model*/}
-            {/*    cursor={true} // Whether to toggle cursor style on drag*/}
-            {/*    snap={false} // Snap-back to center (can also be a spring config)*/}
-            {/*    speed={1.5} // Speed factor*/}
-            {/*    zoom={1} // Zoom factor when half the polar-max is reached*/}
-            {/*    rotation={[0, 0, 0]} // Default rotation*/}
-            {/*    polar={[Math.PI / 12, Math.PI / 4]} // Vertical limits*/}
-            {/*    azimuth={[-Infinity, Infinity]} // Horizontal limits*/}
-            {/*    config={{mass: 1, tension: 0, friction: 0}} // Spring config*/}
-            {/*>*/}
-            <spotLight intensity={2}/>
-            <group scale={0.08}>
-                <TestInstances>
-                    <TestComp/>
-                </TestInstances>
+            {/*<group scale={0.02}>*/}
+            {/*    <TestInstances>*/}
+            {/*        <TestComp/>*/}
+            {/*    </TestInstances>*/}
+            {/*</group>*/}
+            <group scale={0.02}>
+                <TestComp2/>
             </group>
-            {/*</PresentationControls>*/}
         </Canvas>
     )
 }
