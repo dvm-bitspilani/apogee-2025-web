30345852b8692c8faa45275223b8273b3a72483e
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-01-12 14:24:01 +0530

    feat: added new 3d models
    - updated the 3d model
    - played all animations in the model

--- components/Experience/Experience.jsx
+++ components/Experience/Experience.jsx
@@ -1,44 +1,45 @@
-import {Canvas} from "@react-three/fiber";
-import {BakeShadows, Environment, OrbitControls} from "@react-three/drei";
-import {Perf} from "r3f-perf";
-import {TestComp2} from "../TestComp2/TestComp2.jsx";
-import {TestComp, TestInstances} from "../TestComp/TestComp.jsx";
+import { Canvas } from "@react-three/fiber";
+import { BakeShadows, Environment, OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { TestComp2 } from "../TestComp2/TestComp2.jsx";
+import { TestComp, TestInstances } from "../TestComp/TestComp.jsx";
 
 export default function Experience() {
-    return (
-        <Canvas
-            camera={{
-                position: [2, 2, 2],
-                // far: 10
-            }}
-        >
-            <Perf position='top-left'/>
+  return (
+    <Canvas
+      camera={{
+        position: [2, 2, 2],
+        far: 8,
+        near: 0.01,
+      }}
+    >
+      <Perf position="top-left" />
 
-            <OrbitControls
-                maxPolarAngle={Math.PI / 2.5}
-                minPolarAngle={Math.PI / 5}
-                // enablePan={false}
-                // enableZoom={false}
-            />
+      <OrbitControls
+        maxPolarAngle={Math.PI / 2.5}
+        minPolarAngle={Math.PI / 5}
+        // enablePan={false}
+        // enableZoom={false}
+      />
 
-            <Environment
-                files='/environments/kloppenheim_06_puresky_1k.hdr'
-                background={true}
-            />
+      <Environment
+        files="/environments/kloppenheim_06_puresky_1k.hdr"
+        background={true}
+      />
 
-            <BakeShadows/>
+      <BakeShadows />
 
-            {/*<fogExp2 attach="fog" color='#564d65' density={0.03}/>*/}
+      {/*<fogExp2 attach="fog" color='#564d65' density={0.03}/>*/}
 
-            <ambientLight intensity={2}/>
-            {/*<group scale={0.02}>*/}
-            {/*    <TestInstances>*/}
-            {/*        <TestComp/>*/}
-            {/*    </TestInstances>*/}
-            {/*</group>*/}
-            <group scale={0.02}>
-                <TestComp2/>
-            </group>
-        </Canvas>
-    )
+      <ambientLight intensity={2} />
+      {/*<group scale={0.02}>*/}
+      {/*    <TestInstances>*/}
+      {/*        <TestComp/>*/}
+      {/*    </TestInstances>*/}
+      {/*</group>*/}
+      <group scale={0.02}>
+        <TestComp2 />
+      </group>
+    </Canvas>
+  );
 }
