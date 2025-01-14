c815a7b72331fbdd7a56dc9f7bb0830862d80f3d
Manas <manasck14@gmail.com>
2025-01-14 21:07:32 +0530

    feat: created reg-page

---
+++ components/Registration/Registration.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import background from "../../src/assets/regBackground.png";
+
+export default function Registration() {
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          height: "100vh",
+        }}
+      >
+      </div>
+    </>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,14 @@ import { Route, Routes } from "react-router";
 import GlobalLayout from "../layouts/GlobalLayout.jsx";
 import Experience from "../components/Experience/Experience.jsx";
 import Landing from "../components/Landing/Landing.jsx";
+import Registration from "../components/Registration/Registration.jsx";
 
 function App() {
   return (
     <Routes>
       {/*<Route element={<GlobalLayout/>}>*/}
       <Route path="/" element={<Landing />} />
-      <Route
-        path="test"
-        element={
-          <>
-            <h1>Test Page</h1>
-          </>
-        }
-      />
-      {/*</Route>*/}
+      <Route path="/registration" element={<Registration />} />
     </Routes>
   );
 }
