f4dd423d8daaaa1a25e956542ea90c1e93b1f7ce
Samyak Jain <jainsamyak1203@gmail.com>
2025-01-21 21:48:14 +0530

    chore

--- components/Experience/Experience.jsx
+++ components/Experience/Experience.jsx
@@ -1,31 +1,76 @@
-import {
-  BakeShadows,
-  Environment,
-  Html,
-  OrbitControls,
-  useHelper,
-} from "@react-three/drei";
+import { Sky, OrbitControls, useHelper } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import { TestComp2 } from "../TestComp2/TestComp2.jsx";
 import { useRef } from "react";
 import { DirectionalLightHelper, PointLightHelper } from "three";
 import { useControls } from "leva";
-import { Bloom, Noise } from "@react-three/postprocessing";
-import Navbar from "../Landing/Navbar/Navbar.jsx";
-import { mx_fractal_noise_float } from "three/src/nodes/TSL.js";
+import EnergyOrb from "../EnergyOrb/EnergyOrb.jsx";
 
 export default function Experience() {
-  //   const directionalLightHelper = useRef();
+  const directionalLightHelper = useRef();
   const pointLightHelper = useRef();
-  //   useHelper(directionalLightHelper, DirectionalLightHelper, "red");
+  useHelper(directionalLightHelper, DirectionalLightHelper, "red");
   useHelper(pointLightHelper, PointLightHelper, "purple");
+
+  const {
+    skyDistance,
+    sunPosition,
+    inclination,
+    azimuth,
+    rayleigh,
+    mieCoefficient,
+    mieDirectionalG,
+    turbidity,
+  } = useControls({
+    skyDistance: {
+      value: 450000,
+      min: 0,
+      max: 1000000,
+      step: 1000,
+    },
+    sunPosition: {
+      value: [10, 2, 5],
+    },
+    inclination: {
+      value: 0,
+      min: 0,
+      max: 2,
+      step: 0.01,
+    },
+    azimuth: {
+      value: 0.25,
+      min: 0,
+      max: 2,
+      step: 0.01,
+    },
+    rayleigh: {
+      value: 1.5,
+      min: 0,
+      max: 2,
+      step: 0.01,
+    },
+    mieCoefficient: {
+      value: 0.005,
+      min: 0,
+      max: 0.1,
+      step: 0.001,
+    },
+    mieDirectionalG: {
+      value: 0.8,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    turbidity: {
+      value: 8,
+      min: 0,
+      max: 100,
+      step: 1,
+    },
+  });
   return (
     <>
       <Perf position="top-left" />
-
-      {/* <Bloom intensity={4.0} /> */}
-
-      <Noise />
 
       <OrbitControls
       // maxPolarAngle={Math.atan(1.5 / 0.25)}
@@ -34,30 +79,32 @@ export default function Experience() {
       // enableZoom={false}
       />
 
-      <BakeShadows />
-
-      <Environment
+      {/* <Environment
         files="/environments/kloppenheim_06_puresky_1k.hdr"
         environmentIntensity={1}
         backgroundIntensity={1}
-        background={true}
+        // background={true}
+        /> */}
+
+      <Sky
+        distance={skyDistance}
+        sunPosition={sunPosition}
+        inclination={inclination}
+        azimuth={azimuth}
+        rayleigh={rayleigh}
+        mieCoefficient={mieCoefficient}
+        mieDirectionalG={mieDirectionalG}
+        turbidity={turbidity}
+        expo
       />
 
-      <fogExp2 attach="fog" color="#564d65" density={0.3} />
+      <group position={[0, 0.2, 0.075]}>
+        <EnergyOrb color="orange" />
+      </group>
 
-      {/* <directionalLight
-        ref={directionalLightHelper}
-        position={[3, 1, 0]}
-        // target={[0, 1, 0]}
-        intensity={5}
-      /> */}
+      {/* <fogExp2 attach="fog" color="#564d65" density={0.3} /> */}
 
-      {/* <pointLight
-        ref={pointLightHelper}
-        intensity={1}
-        color="blue"
-        position={[1, 1, 0]}
-      /> */}
+      <ambientLight intensity={1} />
 
       <group rotation={[0, -Math.PI / 2, 0]} scale={0.02}>
         <TestComp2 />
