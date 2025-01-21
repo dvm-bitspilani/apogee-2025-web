32785accd53f11af69bcf4897e4ecab097eff502
Manas <manasck14@gmail.com>
2025-01-21 22:46:46 +0530

    setup reg-page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { Route, Routes } from "react-router";
 import GlobalLayout from "../layouts/GlobalLayout.jsx";
 import Experience from "../components/Experience/Experience.jsx";
 import Landing from "../components/Landing/Landing.jsx";
-import Registration from "../components/Registration/Registration.jsx";
+import Register from "../routes/Register.jsx";
 
 function App() {
   return (
     <Routes>
-      {/*<Route element={<GlobalLayout/>}>*/}
       <Route path="/" element={<Landing />} />
-      <Route path="/registration" element={<Registration />} />
+      <Route path="/registration" element={<Register />} />
     </Routes>
   );
 }
