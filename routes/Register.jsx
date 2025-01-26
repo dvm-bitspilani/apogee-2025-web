22ef6d32ac58d673cbed0badd1ec03691d6dc222
Manas <manasck14@gmail.com>
2025-01-26 23:07:17 +0530

    feat: completed mobile view of instructions page

--- routes/Register.jsx
+++ routes/Register.jsx
@@ -1,21 +1,12 @@
 import React, { useState } from "react";
 import Instructions from "../components/Registration/Instructions/Instruction";
-import background from "../src/assets/Register/regBackground.png";
 import styles from "./register.module.scss";
 
 export default function Register() {
 
   return (
     <>
-      <div
-        style={{
-          background: `radial-gradient(40.9% 58.96% at 50% 50%, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.3) 100%), url(${background})`,
-          backgroundSize: "cover",
-          display: "flex",
-          justifyContent: "center",
-        }}
-        className={styles.registerContainer}
-      >
+      <div className={styles.registerContainer}>
         <div className={styles.scrollContainer}>
           <Instructions />
         </div>
--- routes/Register1.jsx
+++ routes/Register1.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import RegForm from "../components/Registration/Form/RegForm";
-import background from "../src/assets/Register/regBackground.png";
 import styles from "./register.module.scss";
 
 export default function Register() {
@@ -21,15 +20,7 @@ export default function Register() {
 
   return (
     <>
-      <div
-        style={{
-          background: `radial-gradient(40.9% 58.96% at 50% 50%, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.3) 100%), url(${background})`,
-          backgroundSize: "cover",
-          display: "flex",
-          justifyContent: "center",
-        }}
-        className={styles.registerContainer}
-      >
+      <div className={styles.registerContainer}>
         <div className={styles.scrollContainer}>
           {hasEmail ? (
             <RegForm />
