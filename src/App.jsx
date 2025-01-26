cec3b7d216345b2a052299063baed34167b77d22
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-01-26 18:10:11 +0530

    feat: traveling ring shader

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import GlobalLayout from "../layouts/GlobalLayout.jsx";
 import Experience from "../components/Experience/Experience.jsx";
 import Landing from "../components/Landing/Landing.jsx";
 import Register from "../routes/Register.jsx";
+import ShaderDev from "../components/ShaderDev/ShaderDev.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/registration" element={<Register />} />
+      <Route path="/shader-dev" element={<ShaderDev />} />
     </Routes>
   );
 }
