adac6aebc5472966970c45c3d2340a5e0b8e1a1d
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-01-27 20:49:56 +0530

    Reapply "feat: glitch effect"
    This reverts commit 19c763d6e657738262a23f9635c845eac2dddbd5.

--- components/EnergyOrb/EnergyOrb.jsx
+++ components/EnergyOrb/EnergyOrb.jsx
@@ -1,7 +1,10 @@
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import { shaderMaterial, Sphere } from "@react-three/drei";
 import * as THREE from "three";
-import { extend } from "@react-three/fiber";
+import { extend, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const GLITCH_STRENGTH_FACTOR = 0.05;
 
 const OuterGlowRadial = shaderMaterial(
   // Uniforms
@@ -10,12 +13,27 @@ const OuterGlowRadial = shaderMaterial(
   },
   // Vertex
   `
+    uniform float uTime;
     varying vec3 vNormal;
     varying vec3 vPosition;
+
+    float random2D(vec2 value)
+    {
+        return fract(sin(dot(value.xy, vec2(12.9898,78.233))) * 43758.5453123);
+    }
 
     void main(){
         // Position
         vec4 modelPosition = modelMatrix * vec4(position , 1.0);
+
+        // Glitch
+        // float glitchTime = uTime - modelPosition.y;
+        // float glitchStrength = sin(glitchTime) + sin(glitchTime * 3.45) + sin(glitchTime * 8.76);
+        // glitchStrength /= 3.0;
+        // glitchStrength = smoothstep(0.3, 1.0, glitchStrength);
+        // glitchStrength *= ${GLITCH_STRENGTH_FACTOR};
+        // modelPosition.x += (random2D(modelPosition.xz + uTime) - 0.5) * glitchStrength;
+        // modelPosition.z += (random2D(modelPosition.zx + uTime) - 0.5) * glitchStrength;
 
         // Final position
         gl_Position = projectionMatrix * viewMatrix * modelPosition;
@@ -55,7 +73,57 @@ const OuterGlowRadial = shaderMaterial(
   `
 );
 
-extend({ OuterGlowRadial });
+const SphereMaterial = shaderMaterial(
+  // Uniforms
+  { uTime: new THREE.Uniform(0), uColor: new THREE.Color("yellow") },
+  // Vertex
+  `
+    uniform float uTime;
+    varying vec3 vNormal;
+    varying vec3 vPosition;
+
+    float random2D(vec2 value)
+    {
+        return fract(sin(dot(value.xy, vec2(12.9898,78.233))) * 43758.5453123);
+    }
+
+    void main(){
+        // Position
+        vec4 modelPosition = modelMatrix * vec4(position , 1.0);
+
+        // Glitch
+        float glitchTime = uTime - modelPosition.y;
+        float glitchStrength = sin(glitchTime) + sin(glitchTime * 3.45) + sin(glitchTime * 8.76);
+        glitchStrength /= 3.0;
+        glitchStrength = smoothstep(0.3, 1.0, glitchStrength);
+        glitchStrength *= ${GLITCH_STRENGTH_FACTOR};
+        modelPosition.x += (random2D(modelPosition.xz + uTime) - 0.5) * glitchStrength;
+        modelPosition.z += (random2D(modelPosition.zx + uTime) - 0.5) * glitchStrength;
+
+        // Final position
+        gl_Position = projectionMatrix * viewMatrix * modelPosition;
+
+        // Model normal
+        vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
+        // Varyings
+        vPosition = modelPosition.xyz;
+        vNormal = modelNormal.xyz;
+    }
+  `,
+  // Fragment
+  `
+    uniform vec3 uColor;
+
+    void main(){
+        gl_FragColor = vec4(uColor, 1.0);
+        #include <tonemapping_fragment>
+        #include <colorspace_fragment>
+    }
+  `
+);
+
+extend({ OuterGlowRadial, SphereMaterial });
 
 export default function EnergyOrb({
   luminanceThreshold = 0.2,
@@ -66,51 +134,50 @@ export default function EnergyOrb({
   lightIntensity = 2,
   isBloom = false,
 }) {
-  if (isBloom) {
-    return (
-      <>
-        <EffectComposer>
-          <Bloom
-            luminanceThreshold={luminanceThreshold}
-            luminanceSmoothing={luminanceSmoothing}
-            intensity={bloomIntensity}
-          />
-        </EffectComposer>
+  const sphereMatRef = useRef();
+  useFrame((state) => {
+    sphereMatRef.current.uniforms.uTime.value = state.clock.elapsedTime;
+  });
+  return (
+    <>
+      {/* <EffectComposer>
+        <Bloom
+          luminanceThreshold={luminanceThreshold}
+          luminanceSmoothing={luminanceSmoothing}
+          intensity={bloomIntensity}
+        />
+      </EffectComposer> */}
 
-        <Sphere args={[0.1, 32, 32]}>
-          <meshStandardMaterial
-            emissive={color}
-            emissiveIntensity={emissiveIntensity}
-            toneMapped={false}
-          />
-          <pointLight
-            position={[0, 0.05, 0]}
-            intensity={lightIntensity}
-            color={color}
-          />
-        </Sphere>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Sphere args={[0.1, 32, 32]} position={[0, 0, 0]}>
-          <meshStandardMaterial
-            color={color}
-            emissive={color}
-            emissiveIntensity={1.5}
-          />
-          <pointLight
-            color={color}
-            position={[0, 0.05, 0]}
-            intensity={lightIntensity}
-          />
-        </Sphere>
+      {/* <Sphere args={[0.1, 32, 32]}>
+        <meshStandardMaterial
+          emissive={color}
+          emissiveIntensity={emissiveIntensity}
+          toneMapped={false}
+        />
+        <pointLight
+          position={[0, 0.05, 0]}
+          intensity={lightIntensity}
+          color={color}
+        />
+      </Sphere> */}
 
-        <Sphere args={[0.12, 32, 32]} position={[0, 0, 0]}>
-          <outerGlowRadial transparent blending={THREE.AdditiveBlending} />
-        </Sphere>
-      </>
-    );
-  }
+      <Sphere args={[0.1, 64, 64]} position={[0, 0, 0]}>
+        <sphereMaterial ref={sphereMatRef} transparent />
+        {/* <meshStandardMaterial
+          color={color}
+          emissive={color}
+          emissiveIntensity={1.5}
+        /> */}
+        <pointLight
+          color={color}
+          position={[0, 0.05, 0]}
+          intensity={lightIntensity}
+        />
+      </Sphere>
+
+      <Sphere args={[0.12, 32, 32]} position={[0, 0, 0]}>
+        <outerGlowRadial transparent blending={THREE.AdditiveBlending} />
+      </Sphere>
+    </>
+  );
 }
