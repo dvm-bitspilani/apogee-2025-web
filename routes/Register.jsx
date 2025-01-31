c8d1fd4ca64b0ba04e847649fe5a93c4793f0f5d
Manas <manasck14@gmail.com>
2025-01-31 23:43:01 +0530

    feat: added back btn to register

--- routes/Register.jsx
+++ routes/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Instructions from "../components/Registration/Instructions/Instruction";
 import styles from "./register.module.scss";
+import BackButton from "../components/Registration/BackButton/BackButton";
 
 export default function Register() {
 
@@ -8,6 +9,9 @@ export default function Register() {
     <>
       <div className={styles.registerContainer}>
           <Instructions />
+      </div>
+      <div className={styles.backbtnContainer}>
+        <BackButton />
       </div>
     </>
   );
