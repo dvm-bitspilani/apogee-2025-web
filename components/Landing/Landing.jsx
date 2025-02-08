95f83376bfd26afcaeace51500826367c046cc26
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-02-08 19:44:43 +0530

    feat: manage pointer events during animation transitions in Experience and Landing components

--- components/Landing/Landing.jsx
+++ components/Landing/Landing.jsx
@@ -5,9 +5,15 @@ import LoadingScreen from "../LoadingScreen/LoadingScreen";
 import Navbar from "./Navbar/Navbar";
 import Overlay from "../Overlay/Overlay";
 import { useLocation } from "react-router";
+import { useSelector } from "react-redux";
 
 export default function Landing() {
   const location = useLocation();
+
+  const isPointerEventsAllowed = useSelector(
+    (state) => state.experienceAnimations.isPointerEventsAllowed
+  );
+
   useEffect(() => {
     const lastLocation = localStorage.getItem("lastLocation");
     if (lastLocation === "/registration") {
@@ -18,6 +24,7 @@ export default function Landing() {
       localStorage.setItem("lastLocation", location.pathname);
     };
   }, []);
+
   return (
     <>
       <Overlay />
@@ -28,6 +35,9 @@ export default function Landing() {
           position: [0, 2.5, 0],
           fov: 50,
           zoom: window.innerWidth < 850 ? 0.5 : 1,
+        }}
+        style={{
+          pointerEvents: isPointerEventsAllowed ? "auto" : "none",
         }}
       >
         <Suspense fallback={<LoadingScreen />}>
