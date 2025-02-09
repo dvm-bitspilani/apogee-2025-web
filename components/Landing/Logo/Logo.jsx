98d2180a2d4c612a7ac0fe461d42019fdc951069
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-02-09 19:21:40 +0530

    fix: add background image to Logo component and adjust styles for improved layout

--- components/Landing/Logo/Logo.jsx
+++ components/Landing/Logo/Logo.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
 import styles from "./logo.module.scss";
 
+import logoBackground from "../../../src/assets/Landing/regbtnLanding.svg";
+
 export default function Logo() {
   const curStage = useSelector((state) => state.experienceAnimations.curStage);
   const isLoading = useSelector(
@@ -19,6 +21,11 @@ export default function Logo() {
             : 0,
       }}
     >
+      <img
+        src={logoBackground}
+        className={styles.bg}
+        alt="background image for logo"
+      />
       <img
         src="/images/logo.svg"
         className={styles.logo}
