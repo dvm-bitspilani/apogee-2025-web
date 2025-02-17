ebb28a69ed3268dd1c4619219d29a4eed49e5aed
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-02-17 22:14:09 +0530

    fix: fixed the contact and contact cards for functionality and dynamic usability

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,122 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { Html } from "@react-three/drei";
+import React from "react";
 import styles from "./contact.module.scss";
 import heading from "../../src/assets/contact.png";
 import ContactCard from "./ContactCard/ContactCard";
-import tarun from "../../src/assets/Contact/tarun.png";
-import kashish from "../../src/assets/Contact/kashish.png";
-import harnoor from "../../src/assets/Contact/harnoor.png";
-import lakshya from "../../src/assets/Contact/lakshya.png";
-import aryan from "../../src/assets/Contact/aryan.png";
-import ahan from "../../src/assets/Contact/ahan.png";
-import bhoomika from "../../src/assets/Contact/bhoomika.png";
-import shubham from "../../src/assets/Contact/shubham.png";
-import pratham from "../../src/assets/Contact/pratham.png";
-import poster from "../../src/assets/contact-poster.png";
-import Preloader from "../Registration/Preloader/Preloader";
+import { CoStAAnData } from "../../utils/data/data";
 
 const Contact = () => {
-  // const [isLoaded, setIsLoaded] = useState(false);
-  const [imagesLoaded, setImagesLoaded] = useState(false);
-  const [showPreloader, setShowPreloader] = useState(true);
-
-  useEffect(() => {
-    const imageUrls = [
-      tarun,
-      kashish,
-      harnoor,
-      lakshya,
-      aryan,
-      ahan,
-      bhoomika,
-      shubham,
-      pratham,
-      poster,
-    ];
-    let loadedCount = 0;
-    imageUrls.forEach((src) => {
-      const img = new Image();
-      img.src = src;
-      img.onload = () => {
-        loadedCount++;
-        if (loadedCount === imageUrls.length) {
-          setTimeout(() => {
-            setImagesLoaded(true);
-            setTimeout(() => {
-              setShowPreloader(false);
-            }, 0);
-          }, 500);
-        }
-      };
-      img.onerror = () => {
-        loadedCount++;
-        if (loadedCount === imageUrls.length) {
-          setImagesLoaded(true);
-        }
-      };
-    });
-  }, []);
   return (
     <>
-      {showPreloader && <Preloader />}
       <div className={styles.wrapper}>
         <div className={styles.heading}>
           <img src={heading} alt="contact us heading" />
           <div className={styles.text}>contact us</div>
         </div>
         <div className={styles.container}>
-          <ContactCard
-            personImg={tarun}
-            name="tarun s"
-            dept="Website, App and Payments"
-          />
-          <ContactCard
-            personImg={harnoor}
-            name="HARNOOR SINGH"
-            dept="Registrations AND 
-Correspondence"
-          />
-          <ContactCard
-            personImg={lakshya}
-            name="LAKSHYA JAIN"
-            dept="Logistics and 
-Operations"
-          />
-          <ContactCard
-            personImg={bhoomika}
-            name="BHOOMIKA JAIN"
-            dept="Online Collaborations 
-and Publicity"
-          />
-          <ContactCard
-            personImg={shubham}
-            name="SHUBHAM SINGH"
-            dept="Reception and 
-Accommodation"
-          />
-          <ContactCard
-            personImg={kashish}
-            name="KASHISH AGRAWAL"
-            dept="Guest Lectures and 
-Paper Presentations"
-          />
-          <ContactCard
-            personImg={pratham}
-            name="pratham sonawane"
-            dept="Sponsorships and 
-Company Collaborations"
-          />
-          <ContactCard
-            personImg={aryan}
-            name="ARYAN KHORANA"
-            dept="General Secretary, 
-Students' Union"
-          />
-          <ContactCard
-            personImg={ahan}
-            name="AHAN BANSAL"
-            dept="President, Students' Union"
-          />
+          {CoStAAnData.map((costaa, index) => {
+            return (
+              <ContactCard
+                personImg={`/images/contact/${costaa.personImg}.png`}
+                name={costaa.name}
+                dept={costaa.dept}
+                phone={costaa.phone}
+                email={costaa.email}
+              />
+            );
+          })}
         </div>
       </div>
     </>
