9ec2d9e8de97098985b6617d8f66632d0fd971f6
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-02-19 20:17:58 +0530

    feat: add Arrows component to enhance experience visuals

--- components/Experience/Experience.jsx
+++ components/Experience/Experience.jsx
@@ -1,10 +1,4 @@
-import {
-  Environment,
-  Float,
-  Html,
-  Image,
-  OrbitControls,
-} from "@react-three/drei";
+import { Environment, Float } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import * as THREE from "three";
 import { useCallback, useEffect, useRef, useState } from "react";
@@ -12,10 +6,8 @@ import { Leva, useControls } from "leva";
 import EnergyOrb from "../EnergyOrb/EnergyOrb.jsx";
 import { CityModel } from "./CityModel/CityModel.jsx";
 import { Blimp } from "./Blimp/Blimp.jsx";
-import down from "../../src/assets/Landing/down.png";
 
 import {
-  curStageUpdate,
   setNavigationStage,
   experienceAnimationsActions,
   reverseAnimation,
@@ -45,13 +37,12 @@ import { getProject } from "@theatre/core";
 import animationStatesDesktop from "../../utils/animation_states/desktop/Landing Project.theatre-project-state.json";
 import animationStatesMobile from "../../utils/animation_states/mobile/Landing Project.theatre-project-state.json";
 import { useThree } from "@react-three/fiber";
-import { ContactBoard } from "./ContactBoard/ContactBoard.jsx";
 import { AboutUs } from "./AboutUs/AboutUs.jsx";
 import { Speakers } from "./Speakers/Speakers.jsx";
-import Arrow from "../Landing/Arrow/Arrow.jsx";
 import { Events } from "./Events/Events.jsx";
 import { Car } from "./Car/Car.jsx";
-import { Train } from "./Train/Train.jsx";
+// import { Train } from "./Train/Train.jsx";
+import Arrows from "./Arrows/Arrows.jsx";
 
 export const landingSheet = getProject("Landing Project", {
   state:
@@ -447,29 +438,7 @@ export default function Experience() {
           />
         </group>
 
-        <Arrow
-          basePosition={[0.85, 0.49, 0.75]}
-          scale={window.innerWidth < 850 ? 0 : 0.06}
-        />
-
-        <Arrow
-          basePosition={[-0.67, 0.49, 0.8]}
-          scale={window.innerWidth < 850 ? 0 : 0.06}
-        />
-
-        <Arrow
-          basePosition={
-            window.innerWidth < 850 ? [0.33, 0.53, 0.65] : [0.55, 0.42, -0.6]
-          }
-          scale={window.innerWidth < 850 ? 0 : 0.06}
-        />
-
-        <Arrow
-          basePosition={
-            window.innerWidth < 850 ? [-0.27, 0.53, 0.8] : [-0.61, 0.47, -0.52]
-          }
-          scale={window.innerWidth < 850 ? 0 : 0.06}
-        />
+        <Arrows />
       </SheetProvider>
     </>
   );
