c7c9b9ef998483d51ff3c78b6abb8de3cda9a671
Manas <manasck14@gmail.com>
2025-02-20 11:16:11 +0530

    chore: removed unused imports from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { Route, Routes, useLocation } from "react-router";
 import GlobalLayout from "../layouts/GlobalLayout.jsx";
-import Experience from "../components/Experience/Experience.jsx";
 import Landing from "../components/Landing/Landing.jsx";
 import ShaderDev from "../components/ShaderDev/ShaderDev.jsx";
-import Contact from "../components/Contact/Contact.jsx";
 import ComingSoon from "../components/ComingSoon/ComingSoon.jsx";
-import About from "../components/About/About.jsx";
 import ReactGA from "react-ga4";
 import Instructions from "../components/Registration/Instructions/Instruction.jsx";
 
@@ -17,9 +14,7 @@ function App() {
       <Route element={<GlobalLayout />}>
         <Route path="/" element={<Landing />} />
         <Route path="/shader-dev" element={<ShaderDev />} />
-        {/* <Route path="/contact" element={<Contact />} /> */}
         <Route path="/registration" element={<Instructions />} />
-        {/* <Route path="/about" element={<About />} /> */}
         <Route path="*" element={<ComingSoon />} />
       </Route>
     </Routes>
