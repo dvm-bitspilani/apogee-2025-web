fa099f5b25bf859ffce80c2ae1e4475d39e7c084
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-02-20 18:47:37 +0530

    feat: add OverlayBackBtn component and integrate into Overlay and ContactPage

---
+++ components/Overlay/OverlayBackBtn/OverlayBackBtn.jsx
@@ -0,0 +1,33 @@
+import styles from "./backBtn.module.scss";
+
+import { useDispatch, useSelector } from "react-redux";
+import { reverseAnimation } from "../../../store/experienceAnimationsSlice/experienceAnimationsSlice";
+import { useCallback } from "react";
+import { useNavigate } from "react-router";
+
+export default function OverlayBackBtn({ isDiffRoute = false }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const animationStage = useSelector(
+    (state) => state.experienceAnimations.animationStage
+  );
+
+  const handleClick = useCallback(() => {
+    if (isDiffRoute) {
+      navigate("/");
+    } else {
+      dispatch(reverseAnimation(animationStage));
+    }
+  }, [dispatch, navigate, animationStage, isDiffRoute]);
+
+  return (
+    <button className={styles.backBtn} onClick={handleClick}>
+      <img
+        src="/images/backBtnLanding.png"
+        alt="back button"
+        draggable={false}
+      />
+    </button>
+  );
+}
