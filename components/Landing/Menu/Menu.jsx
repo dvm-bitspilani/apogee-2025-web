b83781b840b4e6a257fb39e02ab986a2f62ce1c4
Samyak Jain <jainsamyak1203@gmail.com>
2025-03-19 21:28:09 +0530

    feat: add new SVG assets for hamburger menu and update styles in menu module

--- components/Landing/Menu/Menu.jsx
+++ components/Landing/Menu/Menu.jsx
@@ -4,6 +4,22 @@ import logoBackground from "../../../src/assets/Landing/regbtnLanding.svg";
 import hamLeft from "../../../src/assets/Landing/HamMenu/hamLeft.png";
 import hamRight from "../../../src/assets/Landing/HamMenu/hamRight.png";
 import hamMenu from "../../../src/assets/Landing/HamMenu/hamMenu.png";
+import firstGear from "../../../src/assets/Landing/HamMenu/firstGear.svg";
+import secondArc from "../../../src/assets/Landing/HamMenu/secondArc.svg";
+import thirdArc from "../../../src/assets/Landing/HamMenu/thirdArc.svg";
+import firstArrow from "../../../src/assets/Landing/HamMenu/firstArrow.svg";
+import secondArrow from "../../../src/assets/Landing/HamMenu/secondArrow.svg";
+import thirdArrow from "../../../src/assets/Landing/HamMenu/thirdArrow.svg";
+import thirdCircle from "../../../src/assets/Landing/HamMenu/thirdCircle.svg";
+import fourthCircle from "../../../src/assets/Landing/HamMenu/fourthCircle.svg";
+import fifthCircle from "../../../src/assets/Landing/HamMenu/fifthCircle.svg";
+import sixCircle from "../../../src/assets/Landing/HamMenu/sixCircle.svg";
+import secondGear from "../../../src/assets/Landing/HamMenu/secondGear.svg";
+import thirdGear from "../../../src/assets/Landing/HamMenu/thirdGear.svg";
+import gearCircle from "../../../src/assets/Landing/HamMenu/gearCircle.svg";
+import firstGearArc from "../../../src/assets/Landing/HamMenu/firstGearArc.svg";
+import thirdGearArc from "../../../src/assets/Landing/HamMenu/thirdGearArc.svg";
+
 import { useState, useEffect } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
@@ -19,7 +35,6 @@ export default function Menu() {
     const tl = gsap.timeline();
 
     if (isMenuOpen) {
-      // Opening animation
       tl.to(`.${styles.hamLeft}`, {
         x: "0%",
         duration: 0.8,
@@ -96,7 +111,54 @@ export default function Menu() {
         }}
       >
         <div className={styles.ham}>
-          <img className={styles.main} src={hamMenu} alt="ham menu" />
+          {/* <img className={styles.main} src={hamMenu} alt="ham menu" /> */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="31"
+            height="32"
+            viewBox="0 0 31 32"
+            fill="none"
+            className={styles.firstCircle}
+          >
+            <circle
+              cx="15.43"
+              cy="15.6448"
+              r="14.8076"
+              stroke="white"
+              stroke-width="1.24488"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="201"
+            height="81"
+            viewBox="0 0 201 81"
+            fill="none"
+            className={styles.firstArc}
+          >
+            <path
+              d="M200.105 79.9923C165.609 45.7003 122.125 21.1357 74.063 9.51362C49.9452 3.68164 25.3637 1.27533 1 2.18793"
+              stroke="white"
+              stroke-width="2.5"
+            />
+          </svg>
+          <img className={styles.firstGear} src={firstGear} alt="first gear" />
+          <img className={styles.secondArc} src={secondArc} alt="second arc" />
+          <img className={styles.thirdArc} src={thirdArc} alt="third arc" />
+
+          <img className={styles.firstArrow} src={firstArrow} alt="first gear" />
+          <img className={styles.secondArrow} src={secondArrow} alt="second arc" />
+          <img className={styles.thirdArrow} src={thirdArrow} alt="third arc" />
+          <img className={styles.thirdCircle} src={thirdCircle} alt="first gear" />
+          <img className={styles.fourthCircle} src={fourthCircle} alt="second arc" />
+          <img className={styles.fifthCircle} src={fifthCircle} alt="third arc" />
+          <img className={styles.sixCircle} src={sixCircle} alt="first gear" />
+          <img className={styles.secondGear} src={secondGear} alt="second arc" />
+          <img className={styles.thirdGear} src={thirdGear} alt="third arc" />
+          <img className={styles.gearCircle} src={gearCircle} alt="first gear" />
+          <img className={styles.firstGearArc} src={firstGearArc} alt="second arc" />
+          <img className={styles.thirdGearArc} src={thirdGearArc} alt="third arc" />
+
         </div>
         <div className={styles.hamBackground}>
           <button
