affe44b730416423bdf638f0f3d9278278fe95fe
Manas <manasck14@gmail.com>
2025-03-19 23:57:30 +0530

    feat: enhance SpeakersPage layout with heading image and improved styling

--- routes/SpeakersPage/SpeakersPage.jsx
+++ routes/SpeakersPage/SpeakersPage.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 import styles from "./speakersPage.module.scss";
+import heading from "../../src/assets/Speakers/heading.png";
 
 const SpeakerVideo = ({ speakerName = "akbar" }) => {
   const curState = useSelector((state) => state.experienceAnimations.curStage);
@@ -36,10 +37,15 @@ const SpeakerVideo = ({ speakerName = "akbar" }) => {
 
 export default function SpeakersPage() {
   return (
-    <div className={styles.container}>
-      <SpeakerVideo speakerName="akbar" />
-      <SpeakerVideo speakerName="elenla" />
-      <SpeakerVideo speakerName="shivshankar" />
+    <div className={styles.mainContainer}>
+      <div className={styles.heading}>
+        <img src={heading} alt="Speakers" />
+      </div>
+      <div className={styles.Videocontainer}>
+        <SpeakerVideo speakerName="akbar" />
+        <SpeakerVideo speakerName="elenla" />
+        <SpeakerVideo speakerName="shivshankar" />
+      </div>
     </div>
   );
 }
