352d6f025884674bd44da314dd3891d19e82ada9
Manas <manasck14@gmail.com>
2025-03-23 01:07:24 +0530

    feat: add OverlayBackBtn component to SpeakersPage

--- routes/SpeakersPage/Speakers.jsx
+++ routes/SpeakersPage/Speakers.jsx
@@ -2,11 +2,13 @@ import { ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "../../components/Speakers/Experience";
 import Heading from "../../components/Speakers/Heading/Heading";
+import OverlayBackBtn from "../../components/Overlay/OverlayBackBtn/OverlayBackBtn";
 
 export default function SpeakersPage() {
   return (
     <>
       <Heading />
+      <OverlayBackBtn />
       <Canvas>
         <color attach="background" args={["#ececec"]} />
         <ScrollControls pages={30} damping={1}>
