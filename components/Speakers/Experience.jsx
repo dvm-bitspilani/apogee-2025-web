299e19f3144f21c6a5b1ce82d4631aa5f223bca2
Manas <manasck14@gmail.com>
2025-03-22 20:07:43 +0530

    feat: add VideoPlayer component for video playback and remove FloatingText component

--- components/Speakers/Experience.jsx
+++ components/Speakers/Experience.jsx
@@ -1,188 +1,3 @@
-// import { FloatingVideo } from "./FloatingText";
-
-// import { Float, PerspectiveCamera, Text, useScroll } from "@react-three/drei";
-// import { useFrame } from "@react-three/fiber";
-// import { useMemo, useRef } from "react";
-// import * as THREE from "three";
-// import { Group } from "three";
-// import { Airplane } from "./Airplane";
-// import { Background } from "./Background";
-// import { Cloud } from "./Cloud";
-
-// const LINE_NB_POINTS = 1000;
-// const CURVE_DISTANCE = 250;
-// const CURVE_AHEAD_CAMERA = 0.008;
-// const CURVE_AHEAD_AIRPLANE = 0.02;
-// const AIRPLANE_MAX_ANGLE = 35;
-
-// export const Experience = () => {
-//   const curve = useMemo(() => {
-//     return new THREE.CatmullRomCurve3(
-//       [
-//         new THREE.Vector3(0, 0, 0),
-//         new THREE.Vector3(0, 0, -CURVE_DISTANCE),
-//         new THREE.Vector3(100, 0, -2 * CURVE_DISTANCE),
-//         new THREE.Vector3(-100, 0, -3 * CURVE_DISTANCE),
-//         new THREE.Vector3(100, 0, -4 * CURVE_DISTANCE),
-//         new THREE.Vector3(0, 0, -5 * CURVE_DISTANCE),
-//         new THREE.Vector3(0, 0, -6 * CURVE_DISTANCE),
-//         new THREE.Vector3(0, 0, -7 * CURVE_DISTANCE),
-//       ],
-//       false,
-//       "catmullrom",
-//       0.5
-//     );
-//   }, []);
-
-//   const shape = useMemo(() => {
-//     const shape = new THREE.Shape();
-//     shape.moveTo(0, -0.08);
-//     shape.lineTo(0, 0.08);
-
-//     return shape;
-//   }, [curve]);
-
-//   const cameraGroup = useRef();
-//   const scroll = useScroll();
-
-//   useFrame((_state, delta) => {
-//     const scrollOffset = Math.max(0, scroll.offset);
-
-//     const curPoint = curve.getPoint(scrollOffset);
-
-//     // Follow the curve points
-//     cameraGroup.current.position.lerp(curPoint, delta * 24);
-
-//     // Make the group look ahead on the curve
-
-//     const lookAtPoint = curve.getPoint(
-//       Math.min(scrollOffset + CURVE_AHEAD_CAMERA, 1)
-//     );
-
-//     const currentLookAt = cameraGroup.current.getWorldDirection(
-//       new THREE.Vector3()
-//     );
-//     const targetLookAt = new THREE.Vector3()
-//       .subVectors(curPoint, lookAtPoint)
-//       .normalize();
-
-//     const lookAt = currentLookAt.lerp(targetLookAt, delta * 24);
-//     cameraGroup.current.lookAt(
-//       cameraGroup.current.position.clone().add(lookAt)
-//     );
-
-//     // Airplane rotation
-
-//     const tangent = curve.getTangent(scrollOffset + CURVE_AHEAD_AIRPLANE);
-
-//     const nonLerpLookAt = new Group();
-//     nonLerpLookAt.position.copy(curPoint);
-//     nonLerpLookAt.lookAt(nonLerpLookAt.position.clone().add(targetLookAt));
-
-//     tangent.applyAxisAngle(
-//       new THREE.Vector3(0, 1, 0),
-//       -nonLerpLookAt.rotation.y
-//     );
-
-//     let angle = Math.atan2(-tangent.z, tangent.x);
-//     angle = -Math.PI / 2 + angle;
-
-//     let angleDegrees = (angle * 180) / Math.PI;
-//     angleDegrees *= 2.4; // stronger angle
-
-//     // LIMIT PLANE ANGLE
-//     if (angleDegrees < 0) {
-//       angleDegrees = Math.max(angleDegrees, -AIRPLANE_MAX_ANGLE);
-//     }
-//     if (angleDegrees > 0) {
-//       angleDegrees = Math.min(angleDegrees, AIRPLANE_MAX_ANGLE);
-//     }
-
-//     // SET BACK ANGLE
-//     angle = (angleDegrees * Math.PI) / 180;
-
-//     const targetAirplaneQuaternion = new THREE.Quaternion().setFromEuler(
-//       new THREE.Euler(
-//         airplane.current.rotation.x,
-//         airplane.current.rotation.y,
-//         angle
-//       )
-//     );
-//     airplane.current.quaternion.slerp(targetAirplaneQuaternion, delta * 2);
-//   });
-
-//   const airplane = useRef();
-
-//   return (
-//     <>
-//       <directionalLight position={[0, 3, 1]} intensity={0.1} />
-//       {/* <OrbitControls /> */}
-//       <group ref={cameraGroup}>
-//         <Background />
-//         <PerspectiveCamera position={[0, 0, 5]} fov={30} makeDefault />
-//         <group ref={airplane}>
-//           <Float speed={1.5} rotationIntensity={0.5}>
-//             {/* <Airplane
-//               rotation-y={Math.PI / 2}
-//               scale={[0.2, 0.2, 0.2]}
-//               position-y={0.1}
-//             /> */}
-//             <FloatingVideo opacity={0.8} index={1} position-y={0.1}/>
-//           </Float>
-//         </group>
-//       </group>
-//       {/* TEXT */}
-//       <group position={[-3, 0, -100]}>
-//         <FloatingVideo  opacity={0.9} index={0} position={[1.5, -0.5, -2]} />
-//       </group>
-
-//       <group position={[-3, 0, -600]}>
-//         <FloatingVideo opacity={0.8} index={1} position={[3.5, 0.5, -10]} />
-//       </group>
-
-//       {/* LINE */}
-//       <group position-y={-2}>
-//         <mesh>
-//           <extrudeGeometry
-//             args={[
-//               shape,
-//               {
-//                 steps: LINE_NB_POINTS,
-//                 bevelEnabled: false,
-//                 extrudePath: curve,
-//               },
-//             ]}
-//           />
-//           <meshStandardMaterial
-//             color={"white"}
-//             opacity={1}
-//             transparent
-//             envMapIntensity={2}
-//           />
-//         </mesh>
-//       </group>
-
-//       {/* CLOUDS */}
-//       {/* <Cloud scale={[1, 1, 1.5]} position={[-3.5, -1.2, -7]} />
-//       <Cloud scale={[1, 1, 2]} position={[3.5, -1, -10]} rotation-y={Math.PI} />
-//       <Cloud
-//         scale={[1, 1, 1]}
-//         position={[-3.5, 0.2, -12]}
-//         rotation-y={Math.PI / 3}
-//       />
-//       <Cloud scale={[1, 1, 1]} position={[3.5, 0.2, -12]} />
-
-//       <Cloud
-//         scale={[0.4, 0.4, 0.4]}
-//         rotation-y={Math.PI / 9}
-//         position={[1, -0.2, -12]}
-//       />
-//       <Cloud scale={[0.3, 0.5, 2]} position={[-4, -0.5, -53]} />
-//       <Cloud scale={[0.8, 0.8, 0.8]} position={[-1, -1.5, -100]} /> */}
-//     </>
-//   );
-// };
-
 import { Float, PerspectiveCamera, Text, useScroll } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useMemo, useRef } from "react";
@@ -190,11 +5,15 @@ import * as THREE from "three";
 import { Group } from "three";
 import { Airplane } from "./Airplane";
 import { Background } from "./Background";
-import { Cloud, VideoPlayer } from "./Cloud";
+import { Cloud } from "./Cloud";
+import {VideoPlayer} from "./VideoPlayer";
+
+import { OrbitControls } from "@react-three/drei";
+import { Html } from "@react-three/drei";
+
 import akbar from "/videos/akbar.mp4";
 import elenla from "/videos/elenla.mp4";
 import shivshankar from "/videos/shivshankar.mp4";
-import { Html } from "@react-three/drei";
 
 const LINE_NB_POINTS = 1000;
 const CURVE_DISTANCE = 250;
@@ -317,48 +136,17 @@ export const Experience = () => {
           </Float>
         </group>
       </group>
-      {/* TEXT */}
+      
       <group scale={0.8} position={[1, -0.5, -6]}>
-        {/* <Text
-          color="white"
-          anchorX={"left"}
-          anchorY="middle"
-          fontSize={0.22}
-          maxWidth={2.5}
-        >
-          
-        </Text> */}
         <VideoPlayer videoSrc={akbar} />
       </group>
 
       <group scale={1.8} position={[-5, -1, -60]}>
         <VideoPlayer videoSrc={elenla} />
-        {/* <Text
-          color="white"
-          anchorX={"left"}
-          anchorY="top"
-          position-y={-0.66}
-          fontSize={0.22}
-          maxWidth={2.5}
-        >
-          Do you want a drink?{"\n"}
-          We have a wide range of beverages!
-        </Text> */}
       </group>
 
       <group scale={2.8}  position={[-1, -1, -130]}>
         <VideoPlayer videoSrc={shivshankar} />
-        {/* <Text
-          color="white"
-          anchorX={"left"}
-          anchorY="top"
-          position-y={-0.66}
-          fontSize={0.22}
-          maxWidth={2.5}
-        >
-          Do you want a drink?{"\n"}
-          We have a wide range of beverages!
-        </Text> */}
       </group>
 
       <group scale={3.99} position={[-15, -1, -220]}>
@@ -386,24 +174,6 @@ export const Experience = () => {
           />
         </mesh>
       </group>
-
-      {/* CLOUDS */}
-      {/* <Cloud scale={[1, 1, 1.5]} position={[-3.5, -1.2, -7]} />
-      <Cloud scale={[1, 1, 2]} position={[3.5, -1, -10]} rotation-y={Math.PI} />
-      <Cloud
-        scale={[1, 1, 1]}
-        position={[-3.5, 0.2, -12]}
-        rotation-y={Math.PI / 3}
-      />
-      <Cloud scale={[1, 1, 1]} position={[3.5, 0.2, -12]} />
-
-      <Cloud
-        scale={[0.4, 0.4, 0.4]}
-        rotation-y={Math.PI / 9}
-        position={[1, -0.2, -12]}
-      />
-      <Cloud scale={[0.3, 0.5, 2]} position={[-4, -0.5, -53]} />
-      <Cloud scale={[0.8, 0.8, 0.8]} position={[-1, -1.5, -100]} /> */}
     </>
   );
 };
