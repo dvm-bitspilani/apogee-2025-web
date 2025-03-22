a6ae57d4f1fb2ca6c68b8db311e1a22b2f1e8703
Manas <manasck14@gmail.com>
2025-03-23 00:30:08 +0530

    feat: add Heading component for SpeakersPage with responsive styles

--- routes/SpeakersPage/Speakers.jsx
+++ routes/SpeakersPage/Speakers.jsx
@@ -1,10 +1,12 @@
 import { ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "../../components/Speakers/Experience";
+import Heading from "../../components/Speakers/Heading/Heading";
 
 export default function SpeakersPage() {
   return (
     <>
+      <Heading />
       <Canvas>
         <color attach="background" args={["#ececec"]} />
         <ScrollControls pages={30} damping={1}>
