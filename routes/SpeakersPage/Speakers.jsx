0a09cc65dfcb3d6811e2fb1e59aa4f16f9d72f88
Manas <manasck14@gmail.com>
2025-03-26 01:41:32 +0530

    feat: add preloader to SpeakersPage for improved loading experience

--- routes/SpeakersPage/Speakers.jsx
+++ routes/SpeakersPage/Speakers.jsx
@@ -4,17 +4,32 @@ import { SpeakerExperience } from "../../components/Speakers/Experience";
 import Heading from "../../components/Speakers/Heading/Heading";
 import OverlayBackBtn from "../../components/Overlay/OverlayBackBtn/OverlayBackBtn";
 import LoadingScreen from "../../components/LoadingScreen/LoadingScreen";
-import { Suspense } from "react";
+import { Suspense, useState, useEffect } from "react";
 import { Link } from "react-router";
+import Preloader from "../../components/Registration/Preloader/Preloader";
 
 export default function SpeakersPage() {
+  const [showPreloader, setShowPreloader] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowPreloader(false);
+    }, 4000);
+  }, []);
+
   return (
     <>
+      {showPreloader && <Preloader />}
       <Heading />
       <Link to="/">
         <OverlayBackBtn />
       </Link>
-      <Canvas>
+      <Canvas
+        style={{
+          opacity: showPreloader ? 0 : 1,
+          transition: "opacity 0.8s ease-in-out",
+        }}
+      >
         <color attach="background" args={["#000"]} />
         <ScrollControls pages={15} damping={0.4}>
           <Suspense fallback={<LoadingScreen />}>
