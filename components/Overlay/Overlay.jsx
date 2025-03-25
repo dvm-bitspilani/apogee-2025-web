820e12da5e841398a06a00539c9d6e1ee516bf72
Manas <manasck14@gmail.com>
2025-03-26 00:37:43 +0530

    feat: implement responsive behavior for speakers overlay and adjust styles

--- components/Overlay/Overlay.jsx
+++ components/Overlay/Overlay.jsx
@@ -1,10 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./overlay.module.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Contact from "../Contact/Contact";
 import About from "../About/About";
 import Events from "../Events/Events";
-import ComingSoon from "../ComingSoon/ComingSoon";
+// import ComingSoon from "../ComingSoon/ComingSoon";
 import { reverseAnimation } from "../../store/experienceAnimationsSlice/experienceAnimationsSlice";
 import OverlayBackBtn from "./OverlayBackBtn/OverlayBackBtn";
 import { Navigate } from "react-router";
@@ -16,6 +16,21 @@ export default function Overlay() {
   const animationStage = useSelector(
     (state) => state.experienceAnimations.animationStage
   );
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 900);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
 
   useEffect(() => {
     console.log(window.location);
@@ -60,17 +75,20 @@ export default function Overlay() {
       >
         <Events />
       </div>
-      {curState === "speakers" && <Navigate to="/speakers" />}
-      {/* <div
-        className={`${styles.overlayContainer} ${styles.speakerContainer}`}
-        style={
-          curState === "speakers"
-            ? { opacity: 1, pointerEvents: "auto" }
-            : { opacity: 0, pointerEvents: "none" }
-        }
-      >
-        <SpeakersPage />
-      </div> */}
+
+      {!isMobile && curState === "speakers" && <Navigate to="/speakers" />}
+      {isMobile && curState === "speakers" && (
+        <div
+          className={`${styles.overlayContainer} ${styles.speakerContainer}`}
+          style={
+            curState === "speakers"
+              ? { opacity: 1, pointerEvents: "auto" }
+              : { opacity: 0, pointerEvents: "none" }
+          }
+        >
+          <SpeakersPage />
+        </div>
+      )}
       {/* {curState === "speakers" && (
         <iframe
           src={`${window.location.origin}/speakers`}
