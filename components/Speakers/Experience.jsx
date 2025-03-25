c1a7224785ed83e0e37058df30bf55b5cbdc7cef
Manas <manasck14@gmail.com>
2025-03-26 01:48:28 +0530

    fix: clean up commented code and adjust group properties for better readability

--- components/Speakers/Experience.jsx
+++ components/Speakers/Experience.jsx
@@ -27,7 +27,7 @@ export const SpeakerExperience = () => {
   const curve = useMemo(() => {
     return new THREE.CatmullRomCurve3(
       [
-        new THREE.Vector3(-100, 0, 0),  // Start from extreme left
+        new THREE.Vector3(-100, 0, 0), // Start from extreme left
         new THREE.Vector3(-80, 0, -CURVE_DISTANCE),
         new THREE.Vector3(-50, 0, -2 * CURVE_DISTANCE),
         new THREE.Vector3(-10, 0, -3 * CURVE_DISTANCE),
@@ -79,42 +79,42 @@ export const SpeakerExperience = () => {
 
     // Airplane rotation
 
-  //   const tangent = curve.getTangent(scrollOffset + CURVE_AHEAD_AIRPLANE);
+    //   const tangent = curve.getTangent(scrollOffset + CURVE_AHEAD_AIRPLANE);
 
-  //   const nonLerpLookAt = new Group();
-  //   nonLerpLookAt.position.copy(curPoint);
-  //   nonLerpLookAt.lookAt(nonLerpLookAt.position.clone().add(targetLookAt));
+    //   const nonLerpLookAt = new Group();
+    //   nonLerpLookAt.position.copy(curPoint);
+    //   nonLerpLookAt.lookAt(nonLerpLookAt.position.clone().add(targetLookAt));
 
-  //   tangent.applyAxisAngle(
-  //     new THREE.Vector3(0, 1, 0),
-  //     -nonLerpLookAt.rotation.y
-  //   );
+    //   tangent.applyAxisAngle(
+    //     new THREE.Vector3(0, 1, 0),
+    //     -nonLerpLookAt.rotation.y
+    //   );
 
-  //   let angle = Math.atan2(-tangent.z, tangent.x);
-  //   angle = -Math.PI / 2 + angle;
+    //   let angle = Math.atan2(-tangent.z, tangent.x);
+    //   angle = -Math.PI / 2 + angle;
 
-  //   let angleDegrees = (angle * 180) / Math.PI;
-  //   angleDegrees *= 2.4; // stronger angle
+    //   let angleDegrees = (angle * 180) / Math.PI;
+    //   angleDegrees *= 2.4; // stronger angle
 
-  //   // LIMIT PLANE ANGLE
-  //   if (angleDegrees < 0) {
-  //     angleDegrees = Math.max(angleDegrees, -AIRPLANE_MAX_ANGLE);
-  //   }
-  //   if (angleDegrees > 0) {
-  //     angleDegrees = Math.min(angleDegrees, AIRPLANE_MAX_ANGLE);
-  //   }
+    //   // LIMIT PLANE ANGLE
+    //   if (angleDegrees < 0) {
+    //     angleDegrees = Math.max(angleDegrees, -AIRPLANE_MAX_ANGLE);
+    //   }
+    //   if (angleDegrees > 0) {
+    //     angleDegrees = Math.min(angleDegrees, AIRPLANE_MAX_ANGLE);
+    //   }
 
-  //   // SET BACK ANGLE
-  //   angle = (angleDegrees * Math.PI) / 180;
+    //   // SET BACK ANGLE
+    //   angle = (angleDegrees * Math.PI) / 180;
 
-  //   const targetAirplaneQuaternion = new THREE.Quaternion().setFromEuler(
-  //     new THREE.Euler(
-  //       airplane.current.rotation.x,
-  //       airplane.current.rotation.y,
-  //       angle
-  //     )
-  //   );
-  //   airplane.current.quaternion.slerp(targetAirplaneQuaternion, delta * 2);
+    //   const targetAirplaneQuaternion = new THREE.Quaternion().setFromEuler(
+    //     new THREE.Euler(
+    //       airplane.current.rotation.x,
+    //       airplane.current.rotation.y,
+    //       angle
+    //     )
+    //   );
+    //   airplane.current.quaternion.slerp(targetAirplaneQuaternion, delta * 2);
   });
 
   // const airplane = useRef();
@@ -126,7 +126,7 @@ export const SpeakerExperience = () => {
       <group ref={cameraGroup}>
         <Background />
         <PerspectiveCamera position={[0, 0, 5]} fov={30} makeDefault />
-        <group >
+        <group>
           <Float>
             {/* <Airplane
               rotation-y={Math.PI / 2}
@@ -137,23 +137,43 @@ export const SpeakerExperience = () => {
         </group>
       </group>
 
-      <group scale={1.5} position={[-100, -0.6, -10]} rotation={[0, Math.PI / -10, 0]}>
+      <group
+        scale={1.5}
+        position={[-99.3, -0.6, -10]}
+        rotation={[0, Math.PI / -10, 0]}
+      >
         <VideoPlayer videoSrc={vijender} />
       </group>
 
-      <group scale={1.6} position={[-86, -0.6, -40]} rotation={[0, Math.PI / -10, 0]}>
+      <group
+        scale={1.6}
+        position={[-84, -0.6, -40]}
+        rotation={[0, Math.PI / -10, 0]}
+      >
         <VideoPlayer videoSrc={akbar} />
       </group>
 
-      <group scale={1.6} position={[-68, -0.6, -70]} rotation={[0, Math.PI / -6, 0]}>
+      <group
+        scale={1.6}
+        position={[-64.5, -0.6, -70]}
+        rotation={[0, Math.PI / -6, 0]}
+      >
         <VideoPlayer videoSrc={elenla} />
       </group>
 
-      <group scale={1.6} position={[-48, -0.8, -100]} rotation={[0, Math.PI / -6, 0]}>
+      <group
+        scale={1.6}
+        position={[-43, -0.8, -100]}
+        rotation={[0, Math.PI / -6, 0]}
+      >
         <VideoPlayer videoSrc={shivshankar} />
       </group>
 
-      <group scale={1.85} position={[-4, -0.8, -155]} rotation={[0, Math.PI / -5, 0]}>
+      <group
+        scale={1.85}
+        position={[-4, -0.8, -155]}
+        rotation={[0, Math.PI / -5, 0]}
+      >
         <VideoPlayer videoSrc={akbar} />
       </group>
 
