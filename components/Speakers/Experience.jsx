4756871a0ca81041bccf1d83fa61dc688b226739
Manas <manasck14@gmail.com>
2025-03-25 16:52:53 +0530

    feat: comment out unused variables and components in SpeakerExperience

--- components/Speakers/Experience.jsx
+++ components/Speakers/Experience.jsx
@@ -2,10 +2,10 @@ import { Float, PerspectiveCamera, Text, useScroll } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useMemo, useRef } from "react";
 import * as THREE from "three";
-import { Group } from "three";
-import { Airplane } from "./Airplane";
+// import { Group } from "three";
+// import { Airplane } from "./Airplane";
 import { Background } from "./Background";
-import { Cloud } from "./Cloud";
+// import { Cloud } from "./Cloud";
 import { VideoPlayer } from "./VideoPlayer";
 
 import { OrbitControls } from "@react-three/drei";
@@ -17,11 +17,11 @@ import akbar from "/videos/akbar.mp4";
 import elenla from "/videos/elenla.mp4";
 import shivshankar from "/videos/shivshankar.mp4";
 
-const LINE_NB_POINTS = 400;
+// const LINE_NB_POINTS = 400;
 const CURVE_DISTANCE = 50;
 const CURVE_AHEAD_CAMERA = 0.008;
-const CURVE_AHEAD_AIRPLANE = 0.02;
-const AIRPLANE_MAX_ANGLE = 35;
+// const CURVE_AHEAD_AIRPLANE = 0.02;
+// const AIRPLANE_MAX_ANGLE = 35;
 
 export const SpeakerExperience = () => {
   const curve = useMemo(() => {
@@ -79,45 +79,45 @@ export const SpeakerExperience = () => {
 
     // Airplane rotation
 
-    const tangent = curve.getTangent(scrollOffset + CURVE_AHEAD_AIRPLANE);
+  //   const tangent = curve.getTangent(scrollOffset + CURVE_AHEAD_AIRPLANE);
 
-    const nonLerpLookAt = new Group();
-    nonLerpLookAt.position.copy(curPoint);
-    nonLerpLookAt.lookAt(nonLerpLookAt.position.clone().add(targetLookAt));
+  //   const nonLerpLookAt = new Group();
+  //   nonLerpLookAt.position.copy(curPoint);
+  //   nonLerpLookAt.lookAt(nonLerpLookAt.position.clone().add(targetLookAt));
 
-    tangent.applyAxisAngle(
-      new THREE.Vector3(0, 1, 0),
-      -nonLerpLookAt.rotation.y
-    );
+  //   tangent.applyAxisAngle(
+  //     new THREE.Vector3(0, 1, 0),
+  //     -nonLerpLookAt.rotation.y
+  //   );
 
-    let angle = Math.atan2(-tangent.z, tangent.x);
-    angle = -Math.PI / 2 + angle;
+  //   let angle = Math.atan2(-tangent.z, tangent.x);
+  //   angle = -Math.PI / 2 + angle;
 
-    let angleDegrees = (angle * 180) / Math.PI;
-    angleDegrees *= 2.4; // stronger angle
+  //   let angleDegrees = (angle * 180) / Math.PI;
+  //   angleDegrees *= 2.4; // stronger angle
 
-    // LIMIT PLANE ANGLE
-    if (angleDegrees < 0) {
-      angleDegrees = Math.max(angleDegrees, -AIRPLANE_MAX_ANGLE);
-    }
-    if (angleDegrees > 0) {
-      angleDegrees = Math.min(angleDegrees, AIRPLANE_MAX_ANGLE);
-    }
+  //   // LIMIT PLANE ANGLE
+  //   if (angleDegrees < 0) {
+  //     angleDegrees = Math.max(angleDegrees, -AIRPLANE_MAX_ANGLE);
+  //   }
+  //   if (angleDegrees > 0) {
+  //     angleDegrees = Math.min(angleDegrees, AIRPLANE_MAX_ANGLE);
+  //   }
 
-    // SET BACK ANGLE
-    angle = (angleDegrees * Math.PI) / 180;
+  //   // SET BACK ANGLE
+  //   angle = (angleDegrees * Math.PI) / 180;
 
-    const targetAirplaneQuaternion = new THREE.Quaternion().setFromEuler(
-      new THREE.Euler(
-        airplane.current.rotation.x,
-        airplane.current.rotation.y,
-        angle
-      )
-    );
-    airplane.current.quaternion.slerp(targetAirplaneQuaternion, delta * 2);
+  //   const targetAirplaneQuaternion = new THREE.Quaternion().setFromEuler(
+  //     new THREE.Euler(
+  //       airplane.current.rotation.x,
+  //       airplane.current.rotation.y,
+  //       angle
+  //     )
+  //   );
+  //   airplane.current.quaternion.slerp(targetAirplaneQuaternion, delta * 2);
   });
 
-  const airplane = useRef();
+  // const airplane = useRef();
 
   return (
     <>
@@ -126,8 +126,8 @@ export const SpeakerExperience = () => {
       <group ref={cameraGroup}>
         <Background />
         <PerspectiveCamera position={[0, 0, 5]} fov={30} makeDefault />
-        <group ref={airplane}>
-          <Float floatIntensity={1} speed={2.5} rotationIntensity={0.5}>
+        <group >
+          <Float>
             {/* <Airplane
               rotation-y={Math.PI / 2}
               scale={[0.2, 0.2, 0.2]}
