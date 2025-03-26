7229788e88787d27b6d6c2b9d0999278cc949ef8
sumit9722 <shahsumit9722@gmail.com>
2025-03-27 03:18:40 +0530

    add: preloader

---
+++ components/MediaPatners/Preloader/Preloader.jsx
@@ -0,0 +1,28 @@
+import styles from "./preloader.module.scss";
+import loader1 from "../../../src/assets/Register/preloaderimg1.png";
+import loader2 from "../../../src/assets/Register/preloaderimg2.png";
+import loader3 from "../../../src/assets/Register/preloaderimg3.png";
+// import loader4 from "../../../src/assets/Register/preloaderimg4.png";
+
+const Preloader = () => {
+  return (
+    <div className={styles.preloaderContainer}>
+      <div className={styles.imageContainer}>
+        <div className={styles.image1}>
+          <img src={loader1} alt="Loader 1" className={styles.rotatingImage1} />
+        </div>
+        <div className={styles.image2}>
+          <img src={loader2} alt="Loader 2" className={styles.rotatingImage2} />
+        </div>
+        <div className={styles.image3}>
+          <img src={loader3} alt="Loader 3" className={styles.rotatingImage3} />
+        </div>
+        {/* <div className={styles.image4}>
+          <img src={loader4} alt="Loader 4" className={styles.rotatingImage4} />
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
