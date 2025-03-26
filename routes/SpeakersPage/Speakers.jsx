35a264cbdcc83df38ef110db76ecc52491fc93e1
Manas <manasck14@gmail.com>
2025-03-26 18:45:33 +0530

    refactor: remove unnecessary loading screen for SpeakerExperience component

--- routes/SpeakersPage/Speakers.jsx
+++ routes/SpeakersPage/Speakers.jsx
@@ -3,8 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { SpeakerExperience } from "../../components/Speakers/Experience";
 import Heading from "../../components/Speakers/Heading/Heading";
 import OverlayBackBtn from "../../components/Overlay/OverlayBackBtn/OverlayBackBtn";
-import LoadingScreen from "../../components/LoadingScreen/LoadingScreen";
-import { Suspense, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router";
 import Preloader from "../../components/Registration/Preloader/Preloader";
 
@@ -32,9 +31,7 @@ export default function SpeakersPage() {
       >
         <color attach="background" args={["#000"]} />
         <ScrollControls pages={15} damping={0.4}>
-          <Suspense fallback={<LoadingScreen />}>
-            <SpeakerExperience />
-          </Suspense>
+          <SpeakerExperience />
         </ScrollControls>
       </Canvas>
     </>
