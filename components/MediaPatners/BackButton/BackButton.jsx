05656b2735786a2da36484837e47f25b963b12b5
sumit9722 <shahsumit9722@gmail.com>
2025-03-27 17:22:14 +0530

    feat: backbtn preloader and minor fixes

---
+++ components/MediaPatners/BackButton/BackButton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router";
+import styles from "./backbutton.module.scss";
+import backbtn from "../../../src/assets/Register/backbtn.png";
+import mobilebackbtn from "../../../src/assets/Register/mobilebackbtn.png";
+
+export default function BackButton() {
+  return (
+    <Link to={"/"}>
+      {/* <a
+      href="http://localhost:5173/"
+      href="https://apogee2025.netlify.app/"
+      href="https://bits-apogee.org/"
+    > */}
+      <div className={styles.backBtn}>
+        <img src={backbtn} alt="back button"></img>
+      </div>
+      <div className={styles.mobilebackBtn}>
+        <img src={mobilebackbtn} alt="mobile back button"></img>
+      </div>
+      {/* </a> */}
+    </Link>
+  );
+}
